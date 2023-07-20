8c5e8279e718c383dd7eb256af34fae447bc37d5
Satish <satishbabu.uppula14@gmail.com>
2023-07-20 14:20:54 +0530

    frontend 2nd attempt

--- Frontend.sh
+++ Frontend.sh
@@ -1,13 +1,18 @@
-echo "Installing nginx webserver"
+echo "*******************Installing nginx webserver**************************************"
 yum install nginx -y
-echo "enabling system service"
-systemctl enable nginx
-echo "starting system services"
-systemctl start nginx
-echo "Remove the default content that web server is serving"
+echo "********************Copying the nginx configuration file****************************"
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo "*********************Remove the default content that web server is serving**************"
 rm -rf /usr/share/nginx/html/*
-echo "Download the frontend content"
+echo "***********************Download the frontend content************************************"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo "Extract the frontend content"
+echo "***********************Extract the frontend content***************************************"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo "*******************enabling nginx system service******************************************"
+systemctl enable nginx
+echo "********************re-starting system services****************************************"
+systemctl restart nginx
+
+
+
